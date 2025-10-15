c245bfab8d2502f7d1809562159728063798d78a
Alexander <99514363+AlexeanderDev2022@users.noreply.github.com>
2025-10-15 03:53:04 +0000

    cambio para githupPage

--- Cibertec_Frontend/vite.config.js
+++ Cibertec_Frontend/vite.config.js
@@ -5,9 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 80,
+    port: 5173,
     host: true,
-    allowedHosts: ['cibertec-1.onrender.com']
+    allowedHosts: ['cibertec-1.onrender.com',]
 
-  }
+  },
+  base:"/Cibertec"
 })
